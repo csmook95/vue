2818119bb8e335f33a54015d9f6e8cbd74e3592e
unknown <csmook95@gmail.com>
2022-05-25 20:49:33 +0900

    commit

---
+++ learn-vue-js-master/vue-cli/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <AppHeader v-bind:propsdata="str" v-on:renew="renewString"></AppHeader>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+    data() {
+        return {
+            str: "아니"
+        };
+    },
+    components: { AppHeader },
+    methods:{
+      renewString(){
+        this.str='hi'
+      }
+    }
+}
+</script>
+
+<style>
+</style>
